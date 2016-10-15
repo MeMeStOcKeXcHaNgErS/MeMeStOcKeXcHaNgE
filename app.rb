475a34b49229cb6bc4813fb1437d7a58f5715f89
Jack Han <jcdhan1@sheffield.ac.uk>
2016-10-15 22:57:53 +0100

    fixed app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,67 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments' #database configuration
- 
-get '/' do
-      "Hello, World!"
+require 'json'
+require 'omniauth'
+require 'omniauth-twitter'
+
+
+class SinatraApp < Sinatra::Base
+  configure do
+    set :sessions, true
+    set :inline_templates, true
+  end
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  use OmniAuth::Builder do
+    provider :twitter, 'cO23zABqRXQpkmAXa8MRw', 'TwtroETQ6sEDWW8HEgt0CUWxTavwFcMgAwqHdb0k1M'
+    #provider :att, 'client_id', 'client_secret', :callback_url => (ENV['BASE_DOMAIN']
+
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/auth/:provider/callback' do
+    erb "<h1>#{params[:provider]}</h1>
+         <pre>#{JSON.pretty_generate(request.env['omniauth.auth'])}</pre>"
+  end
+
+  get '/auth/failure' do
+    erb "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
+  end
+
+  get '/auth/:provider/deauthorized' do
+    erb "#{params[:provider]} has deauthorized this app."
+  end
+
+  get '/protected' do
+    throw(:halt, [401, "Not authorized\n"]) unless session[:authenticated]
+    erb "<pre>#{request.env['omniauth.auth'].to_json}</pre><hr>
+         <a href='/logout'>Logout</a>"
+  end
+
+  get '/logout' do
+    session[:authenticated] = false
+    redirect '/'
+  end
+
 end
+
+SinatraApp.run! if __FILE__ == $0
+
+__END__
+
+@@ layout
+<html>
+  <head>
+    <link href='http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css' rel='stylesheet' />
+  </head>
+  <body>
+    <div class='container'>
+      <div class='content'>
+        <%= yield %>
+      </div>
+    </div>
+  </body>
+</html>
--- config/environments.rb
+++ config/environments.rb
@@ -1,7 +1,7 @@
 #The environment variable DATABASE_URL should be in the following format:
 ## => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://esinwtech01574.asia.bankofamerica.com/development')
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://zwdtgshltlsaau:VEXbopALY1VWIlHOdCEUXzajU3@ec2-54-247-119-6.eu-west-1.compute.amazonaws.com:5432/d1vs17k9r02m9k'}
   
   ActiveRecord::Base.establish_connection(
       :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
