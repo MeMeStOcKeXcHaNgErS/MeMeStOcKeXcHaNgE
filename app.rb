427d04deef7a06c3ad5d5c6fa5ed7f844136c605
Jack Han <jcdhan1@sheffield.ac.uk>
2016-10-15 22:21:09 +0100

    Harambe

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'activerecord-postgresql-adapater'
 require './config/environments' #database configuration
  
 get '/' do
