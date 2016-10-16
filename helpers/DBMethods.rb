f84a3274c00afe74188962fd5f1ecc1093511bdd
Colin Hutchings <Colin Hutchings>
2016-10-16 04:59:55 +0100

    Should work

--- helpers/DBMethods.rb
+++ helpers/DBMethods.rb
@@ -14,78 +14,47 @@ require 'sqlite3'
 #Methods for Memes
 
 #Return Meme_ID method
-def meme_id_return(name)
-  if name.eql?("Doge")
-    return 01
-  elsif name.eql?('Harambe')
-    return 02
-  elsif name.eql?('Pepe')
-    return 03
-  elsif name.eql?('Spongebob')
-    return 04
-  elsif name.eql?('Xzibit')
-    return 05
-  else
-    puts "error shit"
-    return "error"
-  end	
-end
-
-#Method to return a meme name given name 
-def meme_name_return(name,db)
-  id = meme_id_return(name)
-  memename = db.execute(
-    'SELECT Name FROM Meme WHERE Meme_ID = ?',
-    [id]
-  )
-  return memename
-end
 
 #Method to return a meme description given a name
 def meme_description_return(name,db)
-  id = meme_id_return(name)
   memedscript = db.execute(
-    'SELECT Description FROM Meme WHERE Meme_ID = ?',
-    [id]
+    'SELECT Description FROM Meme WHERE Name = ?',
+    [name]
   )
   return memedscript
 end
 
 #Method to return a meme current price given a name
 def meme_currentprice_return(name,db)
-  id = meme_id_return(name)
   memecurrentprice = db.execute(
-    'SELECT Price FROM Meme WHERE Meme_ID = ?',
-    [id]
+    'SELECT Price FROM Meme WHERE Name = ?',
+    [name]
   )
   return memecurrentprice
 end
 
 #Method to return a meme previous price given a name
 def meme_previousprice_return(name,db)
-  id = meme_id_return(name)
   memepreviousprice = db.execute(
-    'SELECT PreviousPrice FROM Meme WHERE Meme_ID = ?',
-    [id]
+    'SELECT PreviousPrice FROM Meme WHERE Name = ?',
+    [name]
   )
   return memepreviousprice
 end
 
 #Method to change a meme price given a name
 def meme_price_change(name,db,price)
-  id = meme_id_return(name)
   db.execute(
-    'UPDATE Meme SET Price = ? WHERE Meme_ID = ?',
-	[price,id]
+    'UPDATE Meme SET Price = ? WHERE Name = ?',
+	[price,name]
   )
 end
 
 #Method to change a meme previous price given a name
 def meme_previousprice_change(name,db,price)
-  id = meme_id_return(name)
   db.execute(
-    'UPDATE Meme SET PreviousPrice = ? WHERE Meme_ID = ?',
-	[price,id]
+    'UPDATE Meme SET PreviousPrice = ? WHERE Name = ?',
+	[price,name]
   )
 end
 
