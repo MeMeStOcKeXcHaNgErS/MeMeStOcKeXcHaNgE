8c7f16d45f057f05030c76ffc2f321066bd61780
Jack Han <jcdhan1@sheffield.ac.uk>
2016-10-16 06:57:27 +0100

    form

--- controllers/meme.rb
+++ controllers/meme.rb
@@ -3,7 +3,13 @@ require './helpers/DBMethods'
 class MemeController < AppController
   get '/' do
     puts "SOMEONE WANTS MEMES!"
-    @meme_description = meme_description_return(params[:meme_name])
+    @meme_description = meme_description_return(params[:meme_name])[0][0]
+    erb :meme
+  end
+  
+  post '/' do
+    puts "TRANSACTION MADE!"
+    @meme_description = meme_description_return(params[:meme_name])[0][0]
     erb :meme
   end
 end
